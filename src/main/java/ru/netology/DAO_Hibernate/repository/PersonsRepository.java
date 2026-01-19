5ae5f5a28c772b7ebc54421c9b84bf74e7c05363
Aleksey8319 <vologzhin.ka@gmail.com>
2026-01-20 02:06:26 +0800

    Update PersonsRepository.java

--- src/main/java/ru/netology/DAO_Hibernate/repository/PersonsRepository.java
+++ src/main/java/ru/netology/DAO_Hibernate/repository/PersonsRepository.java
@@ -11,36 +11,11 @@ import java.util.List;
 
 
 @Repository
-public class PersonsRepository {
-    @PersistenceContext
-    EntityManager entityManager;
+public interface PersonsRepository extends JpaRepository<Person, Human> {
+    List<Person> findByIgnoreCaseCityOfLiving(String city);
 
-    @Transactional
-    public void repositoryInit() {
+    List<Person> findByHumanAgeLessThanOrderByHumanAgeAsc(int humanAge);
 
-        Human human1 = Human.builder().name("alexey").surname("Bogomolov").age(44).build();
-        Human human2 = Human.builder().name("Alexey").surname("Orlov").age(45).build();
-        Human human3 = Human.builder().name("ALEXEY").surname("Mirin").age(44).build();
-        Human human4 = Human.builder().name("Vasiliy").surname("Stepanov").age(58).build();
-        Human human5 = Human.builder().name("Vasiliy").surname("Utkin").age(52).build();
-
-        Person person1 = Person.builder().human(human1).phoneNumber("+79993412345").cityOfLeaving("Samara").build();
-        entityManager.persist(person1);
-        Person person2 = Person.builder().human(human2).phoneNumber("+79993412346").cityOfLeaving("Moscow").build();
-        entityManager.persist(person2);
-        Person person3 = Person.builder().human(human3).phoneNumber("+79993412347").cityOfLeaving("Irkutsk").build();
-        entityManager.persist(person3);
-        Person person4 = Person.builder().human(human4).phoneNumber("+79993412348").cityOfLeaving("Kemerovo").build();
-        entityManager.persist(person4);
-        Person person5 = Person.builder().human(human5).phoneNumber("+79993412349").cityOfLeaving("Moscow").build();
-        entityManager.persist(person5);
-
-        System.out.println("repositoryInit done");
-    }
-
-    public List<Person> getPersonsByCity(String city) {
-        List<Person> persons = entityManager.createQuery("SELECT e FROM Person e").getResultList();
-        List<Person> filteredPersons = persons.stream().filter(p -> p.getCityOfLeaving().toLowerCase().equals(city.toLowerCase())).toList();
-        return filteredPersons;
-    }
+    List<Person> findByHumanNameIgnoreCaseAndHumanSurnameIgnoreCase(String humanName, String humanSurname);
 }
+
