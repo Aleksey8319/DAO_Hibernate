e3fec1113fa6e6656aea2c8911a41e98ffc675f8
Aleksey8319 <vologzhin.ka@gmail.com>
2026-01-20 02:07:05 +0800

    Update PersonsService.java

--- src/main/java/ru/netology/DAO_Hibernate/service/PersonsService.java
+++ src/main/java/ru/netology/DAO_Hibernate/service/PersonsService.java
@@ -8,16 +8,44 @@ import ru.netology.DAO_Hibernate.repository.PersonsRepository;
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class PersonsService {
-    private final PersonsRepository personsRepository;
 
-    @Autowired
-    public PersonsService(PersonsRepository personsRepository) {
-        this.personsRepository = personsRepository;
-        personsRepository.repositoryInit();
+    private final PersonsRepository repository;
+
+    @Transactional
+    public void run() {
+
+        Human human1 = Human.builder().name("alexey").surname("Bogomolov").age(44).build();
+        Human human2 = Human.builder().name("Alexey").surname("Orlov").age(45).build();
+        Human human3 = Human.builder().name("ALEXEY").surname("Mirin").age(44).build();
+        Human human4 = Human.builder().name("Vasiliy").surname("Stepanov").age(58).build();
+        Human human5 = Human.builder().name("Vasiliy").surname("Utkin").age(52).build();
+
+        Person person1 = Person.builder().human(human1).phoneNumber("+79993412345").cityOfLiving("Samara").build();
+        repository.save(person1);
+        Person person2 = Person.builder().human(human2).phoneNumber("+79993412346").cityOfLiving("Moscow").build();
+        repository.save(person2);
+        Person person3 = Person.builder().human(human3).phoneNumber("+79993412347").cityOfLiving("Irkutsk").build();
+        repository.save(person3);
+        Person person4 = Person.builder().human(human4).phoneNumber("+79993412348").cityOfLiving("Kemerovo").build();
+        repository.save(person4);
+        Person person5 = Person.builder().human(human5).phoneNumber("+79993412349").cityOfLiving("Moscow").build();
+        repository.save(person5);
+
     }
 
     public List<Person> getPersonsByCity(String city) {
-        return personsRepository.getPersonsByCity(city);
+        return repository.findByIgnoreCaseCityOfLiving(city);
     }
+
+    public List<Person> getPersonsByAge(int age) {
+        return repository.findByHumanAgeLessThanOrderByHumanAgeAsc(age);
+    }
+
+    public List<Person> getPersonsByNameSurname(String name, String surname) {
+        return repository.findByHumanNameIgnoreCaseAndHumanSurnameIgnoreCase(name, surname);
+    }
+
 }
+
